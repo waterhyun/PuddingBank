7eb256d9b6c89102a50ce13ae450e5905d3db9bb
ssafy-suhyun <1eesuhyun521@gmail.com>
2024-11-18 13:44:06 +0900

    first commit

---
+++ django-pjt/finance_services/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+
+# urlpatterns = [
+#     path('finance_services/', views.article_list),
+#     path('finance_services/<int:article_pk>/', views.article_detail),
+# ]
