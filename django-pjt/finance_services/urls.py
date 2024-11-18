9f2ce9722228407f9267bda46daf6228b2b8035b
ssafy-suhyun <1eesuhyun521@gmail.com>
2024-11-19 08:53:10 +0900

    finance_services 앱 삭제

--- django-pjt/finance_services/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('finance_services/', views.article_list),
-    path('finance_services/articles/', views.article_list),
-    # path('finance_services/<int:article_pk>/', views.article_detail),
-]
