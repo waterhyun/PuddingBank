3ff151d4fd830bea27a37ede6877b33da914db64
ssafy-suhyun <1eesuhyun521@gmail.com>
2024-11-19 18:19:51 +0900

    '|' 추가

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -8,7 +8,7 @@ import { RouterLink, RouterView } from 'vue-router'
       <nav>
         <RouterLink to="/">Home</RouterLink> |
         <RouterLink to="/articles">게시판</RouterLink> |
-        <RouterLink to="/banks">은행 찾기</RouterLink>
+        <RouterLink to="/banks">은행 찾기</RouterLink> |
         <RouterLink to="/exchanges">환율계산기</RouterLink>
       </nav>
     </div> <RouterView/>
